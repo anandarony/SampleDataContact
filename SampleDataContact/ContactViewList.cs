aec00ea56929f48940ff8682d903f1f0b1866b38
Rony <rony@itmagnet.com.au>
2024-09-09 15:20:16 +0600

    Add project files.

---
+++ SampleDataContact/ContactViewList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleDataContact
+{
+    public class ContactViewList
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ SampleDataContact/Program.cs
@@ -0,0 +1,25 @@
+﻿using Bogus;
+using SampleDataContact;
+
+public class Program
+{    
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");        
+        var userFaker = new Faker<ContactViewList>()
+            .RuleFor(u => u.FirstName, f => f.Name.FirstName())
+            .RuleFor(u => u.LastName, f => f.Name.LastName())
+            .RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+            .RuleFor(u => u.DateOfBirth, f => f.Date.Past(30, DateTime.Now.AddYears(-18))) // Ages between 18 and 48
+            .RuleFor(u => u.PhoneNumber, f => f.Phone.PhoneNumber());
+
+        // Generate a list of fake users
+        List<ContactViewList> users = userFaker.Generate(10);
+
+        // Print the generated users
+        foreach (var user in users)
+        {
+            Console.WriteLine($"Name: {user.FirstName} {user.LastName}, Email: {user.Email}, DOB: {user.DateOfBirth}, Phone: {user.PhoneNumber}");
+        }
+    }
+}
